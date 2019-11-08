391076c6efe468dc5bec575acb4dce870933ef35
lifegpc <g1710431395@gmail.com>
2019-11-08 21:37:33 +0800

    对文件列表进行过滤

--- file/__init__.py
+++ file/__init__.py
@@ -1,2 +1,9 @@
 from file.info import getinfo,getinfox,printinfo
 from file.dir import getinfod,printinfod,listd
+from file.filter import listf,listfd,listff
+#对后缀名过滤
+LX_FILTER=0
+#对文件名进行正则过滤
+TEXT_FILTER=1
+#对后缀名进行过滤时，保留无后缀名名文件
+ILX_FILTER=2
