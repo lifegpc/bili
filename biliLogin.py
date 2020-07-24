8ada67127ef860ff1ebb144066af834ce2f72458
lifegpc <g1710431395@gmail.com>
2020-07-24 13:48:04 +0800

    尝试修复BUG

--- biliLogin.py
+++ biliLogin.py
@@ -113,7 +113,7 @@ def login2(r:requests.Session):
                     sa.append({'name':i['name'],'value':i['value'],'domain':'.bilibili.com','path':'/'})
                 return sa
             elif re['code']==-105 :
-                re=r.get('https://passport.bilibili.com/captcha',headers={'Host': "passport.bilibili.com"},decode_level=1)
+                re=r.get('https://passport.bilibili.com/captcha',headers={'Host': "passport.bilibili.com"}).content
                 cp=scap(r,re)
                 if cp:
                     print(lan['OUTPUT2'].replace('<result>',str(cp)))#登录验证码识别结果：<result>
