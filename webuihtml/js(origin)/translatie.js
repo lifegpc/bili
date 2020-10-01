09a3c3cb67fd7d2a7222c02c938fb5a994386ffd
lifegpc <g1710431395@gmail.com>
2020-10-01 12:23:04 +0800

    translatie.js 支持监听元素属性修改

--- webuihtml/js(origin)/translatie.js
+++ webuihtml/js(origin)/translatie.js
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 /**@typedef {Object} TransObj
  * @property {()=>void} deal 处理新的元素
+ * @property {MutationObserver} observer DOM检测器
 */
 /**@type {TransObj} 存放翻译内容*/
 var transobj = Object();
@@ -43,6 +44,81 @@ var gettrans = (name, list, i, dealwith) => {
         }
     })
 }
+/**获得新的翻译
+ * @param {string} name
+ * @param {()=>{}} f
+*/
+function gettrans2(name, f) {
+    var param = {};
+    var hl = new URL(window.location.href).searchParams.get('hl');
+    if (hl != null) param['hl'] = hl;
+    $.getJSON('/translate/' + name, param, (ob, stat) => {
+        if (stat != "success") return;
+        if (ob.code != 0) console.warn('Can not get translate file "' + name + '".');
+        transobj[name] = ob.dict;
+        f();
+    })
+}
+transobj.observer = new MutationObserver((mutationsList, observer) => {
+    mutationsList.forEach((v) => {
+        if (v.type == "attributes") {
+            /**@type {HTMLElement}*/
+            var o = v.target;
+            function dealwithtar(o) {
+                var a = o.getAttribute('trans');
+                if (a != null) {
+                    var l = a.split(' ');
+                    if (l.length > 2) {
+                        l = [l[0], l.slice(1).join(" ")];
+                    }
+                    if (transobj.hasOwnProperty(l[0])) {
+                        /**@type {Object} 翻译词典*/
+                        var obj2 = transobj[l[0]];
+                        if (obj2.hasOwnProperty(l[1])) {
+                            /**@type {string}*/
+                            var t = obj2[l[1]]
+                            /**@type {Array} */
+                            var t_list = [...t.matchAll(/<([^>]+)>/g)]
+                            if (t_list.length == 0) o.innerText = t;
+                            else {
+                                for (var j = 0; j < t_list.length; j++) {
+                                    var key1 = t_list[j][0];
+                                    var key2 = t_list[j][1];
+                                    if (o.hasAttribute(key2)) {
+                                        t = t.replace(key1, o.getAttribute(key2));
+                                    }
+                                    else {
+                                        console.warn(o);
+                                        console.warn('This object do not contain ' + key2 + ' attributes.')
+                                    }
+                                }
+                                o.innerText = t;
+                            }
+                            if (o.hasAttribute('n')) {
+                                o.setAttribute('n', t);
+                            }
+                            if (o.classList.has('tvalue')) {
+                                o.setAttribute('value', t);
+                            }
+                        }
+                        else {
+                            console.warn(o);
+                            console.warn('This object do not have translated text.')
+                        }
+                    }
+                    else {
+                        gettrans2(l[0], () => { dealwithtar(o); });
+                    }
+                }
+                else {
+                    console.warn(o);
+                    console.warn('This object do not have trans attribute.')
+                }
+            }
+            dealwithtar(o);
+        }
+    })
+})
 transobj.deal = () => {
     /**@type {HTMLCollectionOf<HTMLElement>}*/
     var list = document.getElementsByClassName('trans');
@@ -93,6 +169,7 @@ transobj.deal = () => {
                         o.setAttribute('value', t);
                     }
                     o.setAttribute('t', 1);//标记为已翻译过
+                    transobj.observer.observe(o, { 'attributes': true, 'characterData': false, 'childList': false, 'subtree': false })
                     if (i < le - 1) {
                         setTimeout(() => { dealwith(list[i + 1], i + 1); }, 10);
                     }
