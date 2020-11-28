01225e4d52d55e6dd8a5afab1353eb94ef9a19a0
lifegpc <g1710431395@gmail.com>
2020-11-28 21:59:23 +0800

    支持b23.tv/au<auid>

--- start.py
+++ start.py
@@ -174,7 +174,7 @@ def main(ip={}):
     else :
         re=search(r'([^:]+://)?(www\.)?(space\.)?(vc\.)?(m\.)?(live\.)?bilibili\.com/(s?/?video/av([0-9]+))?(s?/?video/(bv[0-9A-Z]+))?(bangumi/play/(ss[0-9]+))?(bangumi/play/(ep[0-9]+))?(([0-9]+)/favlist(\?(.+)?)?)?(([0-9]+)/channel/(index)?(detail\?cid=([0-9]+))?)?(([0-9]+)/video(\?(.+)?)?)?(bangumi/media/md([0-9]+))?(video/([0-9]+))?(mobile/detail\?vc=([0-9]+))?(record/([^\?]+))?(cheese/play/ss([0-9]+))?(cheese/play/ep([0-9]+))?(v/cheese/mine/list)?(cheese/mine/list)?([0-9]+)?(audio/au([0-9]+))?',inp,I)
         if re==None :
-            re=search(r'([^:]+://)?(www\.)?b23\.tv/(av([0-9]+))?(bv[0-9A-Z]+)?(ss[0-9]+)?(ep[0-9]+)?',inp,I)
+            re=search(r'([^:]+://)?(www\.)?b23\.tv/(av([0-9]+))?(bv[0-9A-Z]+)?(ss[0-9]+)?(ep[0-9]+)?(au([0-9]+))?',inp,I)
             if re==None :
                 re=search(r"[^:]+://",inp)
                 if re==None :
@@ -215,6 +215,11 @@ def main(ip={}):
                     ep=True
                     if log and not logg.hasf():
                         logg.openf(f"log/EP{inp}_{round(time())}.log")
+                elif re[7]:
+                    au = True
+                    auid = int(re[8])
+                    if log and not logg.hasf():
+                        logg.openf(f"log/AU{auid}_{round(time())}.log")
                 else :
                     re=search(r"[^:]+://",inp)
                     if re==None :
